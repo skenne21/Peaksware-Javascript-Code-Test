284e64249c2c7d3feafb82abdd1cf7689c83547a
Sabrina Kennedy <skenne21@gmail.com>
2018-05-20 13:00:38 -0600

    Initalcommit Add file structure, and set up googleMaps api with App and MapsContianer. Map rendering onto page

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+// import  { covertTime } from './cleaner';
+import MapContainer from './MapContainer/';
+import { GoogleApiWrapper } from 'google-maps-react';
+import { googleMapApiKey } from './apiKey.js';
+import PropTypes from 'prop-types';
+const  workoutData = require('./cleaners/workout-data.json');
+
+
+
+
+
+class App extends Component {
+  componentDidMount () {
+    // const data = covertTime(workoutData.samples, 20)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Fitness Tracker</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <MapContainer 
+          google={this.props.google}
+        />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  google: PropTypes.object,
+};
+
+export default  GoogleApiWrapper({ apiKey: googleMapApiKey})(App) ;
---
+++ src/cleaners/workoutDataCleaner.js
@@ -0,0 +1,43 @@
+
+
+// findBest(samples, 20)
+
+// export const covertTime = (workoutData, timeParams) => {
+//   const timeBlocks = [];
+//   const totalMill = 60000 * timeParams
+//   for ( let i = 0; i < workoutData.length; i++) {
+//     for( let j = 1; j < workoutData.length - totalMill ; j++) {
+//       let session = workoutData[i];
+//       let nextSession = workoutData[j];
+//       let power = 0;
+//     // while (session.millisecondOffset + nextSession.millisecondOffset === totalMill) {
+
+//     }
+//     //   let mintues = Math.floor(session.millisecondOffset / 60000)
+//     //   session.millisecondOffset = mintues
+//     // // console.log(session)
+//     // // console.log(session)
+//     // // console.log(timeParams)
+//     // if (session.millisecondOffset <= timeParams) {
+//     //   let time = session.millisecondOffset + nextSession.millisecondOffset
+//     //   while( session.millisecondOffset + nextSession.millisecondOffset !== timeParams && time !== 20) {
+//     //     let time = session.millisecondOffset + nextSession.millisecondOffset;
+//     //     timeBlocks.push(session, nextSession);
+//     //   }
+//     // }
+
+//         // console.log(timeBlocks)
+//     // if (session.mintues > timeParams) {
+//     //   console.log(session.mintues / timeParams)
+//     // }
+//     }
+    
+    
+
+//   }
+
+
+
+// need to loop through array to convert time into milliseconds
+
+
